caf84e0b5bde4acb3cb084df823e861749d6234d
Enamul Haque <shuvoworld@gmail.com>
2024-10-18 12:04:08 +0600

    rr

--- application/views/example.php
+++ application/views/example.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="utf-8" />
+    <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<?php 
+    <?php 
 foreach($css_files as $file): ?>
-	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
-<?php endforeach; ?>
+    <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+    <?php endforeach; ?>
 </head>
+
 <body>
-	<div style='height:20px;'></div>  
+    <div style='height:20px;'></div>
     <div style="padding: 10px">
-		<?php echo $output; ?>
+        <?php echo $output; ?>
     </div>
     <?php foreach($js_files as $file): ?>
-        <script src="<?php echo $file; ?>"></script>
+    <script src="<?php echo $file; ?>"></script>
     <?php endforeach; ?>
 </body>
+
 </html>
